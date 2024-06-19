5677478b10d1054c68557e8b6bcbe66fb0bab7ed
Nail Minnemullin <n.minnemullin@innopolis.university>
2024-06-19 20:54:05 +0300

    Added basic GX functionality

--- src/data.py
+++ src/data.py
@@ -1,32 +1,17 @@
-# Add a function sample_data in a module src/data.py to read the data file and take a sample. Use Hydra to manage the data url in the configs and any configurations needed like the size of the sample, the name of the dataset,…etc. You need to define a structure for project configurations and use Hydra to read them from the source code. The function should read the data url from config files using Hydra and sample the data. Then, it stores the data samples in data/samples folder as sample.csv where the sample size is 20% of the total rows in the whole data. The sample size can be added to the config files too. The idea here is to make your project maintainable and ready for any changes with minimal modifications (DRY principle). For each new sample added, version the data using dvc to not lose any data samples.
-# Run the function sample_data and it should generate a sample. The function should overwrite the file sample.csv if it already exists. You can manage the data versions using dvc.
-import json
-
 import hydra
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import DictConfig
 import pandas as pd
 import os
 
 
 # TODO add comments
 
-def create_kaggle_json(username, key):
-    kaggle_credentials = {
-        "username": username,
-        "key": key
-    }
-    os.makedirs("~/.kaggle")
-
-    # Path to the kaggle.json file
-    f = open("~/.kaggle/kaggle.json", 'w')
-    f.write("Now the file has more content!")
-    f.close()
-
 
 @hydra.main(version_base=None, config_path="../configs", config_name="main")
 def sample_data(cfg: DictConfig = None):
     os.environ['KAGGLE_USERNAME'] = 'nai1ka'
     os.environ['KAGGLE_KEY'] = 'a44922ed88b8f8675c22ef8269d2232c'
+
     import kaggle
     from kaggle.api.kaggle_api_extended import KaggleApi
     api = KaggleApi()
@@ -46,5 +31,31 @@ def sample_data(cfg: DictConfig = None):
     os.rmdir("../data/temp")
 
 
+from great_expectations.data_context import FileDataContext
+
+
+def validate_initial_data():
+    context = FileDataContext(context_root_dir="../services/gx")
+    ds = context.sources.add_or_update_pandas(name="pandas_datasource")
+    da1 = ds.add_csv_asset(
+        name="csv_file",
+        filepath_or_buffer="../data/samples/sample.csv"
+    )
+
+    batch_request = da1.build_batch_request()
+    batches = da1.get_batch_list_from_batch_request(batch_request)
+    for batch in batches:
+        print(batch.batch_spec)
+    context.add_or_update_expectation_suite("initial_data_validation")
+    validator = context.get_validator(
+        batch_request=batch_request,
+        expectation_suite_name="initial_data_validation"
+    )
+
+    ex1 = validator.expect_column_values_to_be_unique(column="id")
+    assert ex1['success']
+
+
 if __name__ == "__main__":
-    sample_data()
+    # sample_data()
+    validate_initial_data()
