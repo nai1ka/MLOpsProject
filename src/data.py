d4f9dc6c8ebec5b928abbe567d25ce5d43be13f8
Nail Minnemullin <n.minnemullin@innopolis.university>
2024-06-21 12:24:23 +0300

    Changed the way how sample is generated

--- src/data.py
+++ src/data.py
@@ -9,30 +9,16 @@ import os
 
 @hydra.main(version_base=None, config_path="../configs", config_name="main")
 def sample_data(cfg: DictConfig = None):
-    os.environ['KAGGLE_USERNAME'] = 'nai1ka'
-    os.environ['KAGGLE_KEY'] = 'a44922ed88b8f8675c22ef8269d2232c'
-
-    import kaggle
-    from kaggle.api.kaggle_api_extended import KaggleApi
-    api = KaggleApi()
-    api.authenticate()
-    data_url = cfg.data.url
-    sample_size = cfg.data.sample_size
     dataset_name = cfg.data.dataset_name
+    output_name = cfg.data.output_name
 
-    kaggle.api.dataset_download_files(data_url, path="../data/temp", unzip=True)
-
-    df = pd.read_csv("../data/temp/" + dataset_name)
-    sample_df = df.sample(frac=sample_size, random_state=1)
-    sample_df.to_csv("../data/samples/sample.csv", index=False)
-
-    # Clean up temporary directory
-    os.remove("../data/temp/" + dataset_name)
-    os.rmdir("../data/temp")
+    df = pd.read_csv("../data/" + dataset_name)
+    sample_size = int(cfg.data.sample_size * len(df))
+    sample_df = df[0:sample_size]
+    sample_df.to_csv("../data/samples/"+output_name, index=False)
 
 
 from great_expectations.data_context import FileDataContext
-
 
 def validate_initial_data():
     context = FileDataContext(context_root_dir="../services/gx")
@@ -43,9 +29,7 @@ def validate_initial_data():
     )
 
     batch_request = da1.build_batch_request()
-    batches = da1.get_batch_list_from_batch_request(batch_request)
-    for batch in batches:
-        print(batch.batch_spec)
+
     context.add_or_update_expectation_suite("initial_data_validation")
     validator = context.get_validator(
         batch_request=batch_request,
@@ -57,5 +41,5 @@ def validate_initial_data():
 
 
 if __name__ == "__main__":
-    # sample_data()
-    validate_initial_data()
+    sample_data()
+    #validate_initial_data()
