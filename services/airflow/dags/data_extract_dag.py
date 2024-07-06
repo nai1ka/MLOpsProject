aff6052dd5ce196440e16dd11e7fcfb5207bc17a
Nail <nail.minnemullin@mail.ru>
2024-07-06 14:13:12 +0000

    airflow done

---
+++ services/airflow/dags/data_extract_dag.py
@@ -0,0 +1,63 @@
+from airflow import DAG
+
+from datetime import timedelta, datetime
+import pandas as pd
+import great_expectations as ge
+import os
+import subprocess
+import yaml
+from hydra import compose, initialize,initialize_config_dir
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+
+from sample_data import sample_data
+from validate_data import validate_initial_data
+def take_sample():
+    # TODO maybe find a better way to navigate
+    path = os.environ["PYTHONPATH"]
+    os.chdir(path)
+    initialize_config_dir(config_dir=f"{path}/../configs")
+    cfg = compose(config_name="main")
+    # Run sample_data function with Hydra configuration
+    sample_data(cfg)
+
+def validate():
+    path = os.environ["PYTHONPATH"]
+    os.chdir(path)
+    initialize_config_dir(config_dir=f"{path}/../configs")
+    cfg = compose(config_name="main")
+    validate_initial_data(cfg)
+
+
+
+dag = DAG(
+    'data_extract_dag',
+    schedule=timedelta(minutes=5),
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+)
+
+extract_task = PythonOperator(
+    task_id='extract_data',
+    python_callable=take_sample,
+    dag=dag,
+)
+
+validate_task = PythonOperator(
+    task_id='validate_data',
+    python_callable=validate,
+    dag=dag,
+)
+version_task = BashOperator(
+    task_id='version_data',
+    bash_command='cd $AIRFLOW_HOME; bash ../../scripts/version_data.sh ',
+    dag=dag,
+)
+
+load_task = BashOperator(
+    task_id='load_data',
+    bash_command='cd $PYTHONPATH; dvc push ',
+    dag=dag,
+)
+
+extract_task >> validate_task >> version_task >> load_task
--- src/sample_data.py
+++ src/sample_data.py
@@ -8,15 +8,15 @@ import sys
 @hydra.main(version_base=None, config_path="../configs", config_name="main")
 def sample_data(cfg: DictConfig = None):
     """Create a sample of the dataset"""
-    dataset_name = cfg.data.dataset_name
-    output_name = cfg.data.output_name
-    sample_version = cfg.data.sample_version
+    dataset_name = cfg.dataset_name
+    output_name = cfg.output_name
+    sample_version = cfg.sample_version
 
     # Load the dataset
     df = pd.read_csv("../data/" + dataset_name)
 
     # Calculate the sample size
-    sample_size = int(cfg.data.sample_size * len(df))
+    sample_size = int(cfg.sample_size * len(df))
 
     # Create the sample
     sample_df = df[(sample_version - 1) * sample_size:sample_version * sample_size]
