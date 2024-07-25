0504f4626ac45a970677543cf3d1028105369f83
nai1ka <nail.minnemullin@mail.ru>
2024-07-25 14:43:26 +0000

    added 2 new models

--- src/main.py
+++ src/main.py
@@ -16,12 +16,14 @@ def run(cfg):
     train_data_version = cfg.train_data_version
     X, y = extract_features(name = "features_target", version=train_data_version)
 
+    print("Train dataset: ", X.shape, train_data_version)
     # Split the data into training and validation sets
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=cfg.test_size, random_state=cfg.random_state) 
 
     # Extract features and target for test data using the specified version
     test_data_version = cfg.test_data_version
     X_test, y_test = extract_features(name = "features_target", version=test_data_version)
+    print("Test dataset: ", X_test.shape, test_data_version)
 
     # Train the model using the training data and provided configuration
     gs = train(X_train, y_train, cfg=cfg)
